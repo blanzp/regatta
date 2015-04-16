962c283a23142c3ba7de0b31c55143a5fe223d9f
blanzp <paul.blanz@gmail.com>
2015-04-16 00:04:04 -0400

    upload controller

---
+++ src/main/python/static/js/uploadCtrl.js
@@ -0,0 +1,26 @@
+regattaApp
+.controller('UploadController', function($scope, FileUploader) {
+	$scope.hideSpinner = true;
+
+	var uploader = $scope.uploader = new FileUploader({url: '/api/loadrml'});
+	uploader.onCompleteItem = function(fileItem, response, status, headers) {
+		console.info('onCompleteItem', fileItem, response, status, headers);
+		$scope.status = "RML file processing complete.  Ready to go"
+		$scope.hideSpinner = true;
+	};
+	uploader.onProgressItem = function(fileItem, progress) {
+		console.info('onProgressItem', fileItem, progress);
+	};
+	uploader.onBeforeUploadItem = function(item) {
+		$scope.hideSpinner = false;
+		$scope.status = "Processing regatta file......this will take a minute	"
+		console.info('onBeforeUploadItem', item);
+	};
+	uploader.onAfterAddingFile = function(fileItem) {
+		console.info('onAfterAddingFile', fileItem);
+		if ( uploader.queue.length > 1 ) {
+			$scope.status = "Only upload 1 file"
+			uploader.queue[1].remove();
+		}
+	};
+});
